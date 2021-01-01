d9402cf34cdcd335be9323ac267e31243dde0c48
Almost Engineer <bitsecondal@gmail.com>
2021-01-01 11:30:08 -0600

    Completed new year countdown challenge

---
+++ newyearcountdown/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Almostengr.NewYearCountdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting countdown!");
+
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(TimeSpan.FromSeconds(1)); // Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Happy New Year {0}", DateTime.Now.Year);
+        }
+    }
+}
