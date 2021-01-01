559313576b32f9eb555d701dfc6841e3e39378a4
Almost Engineer <bitsecondal@gmail.com>
2020-12-31 22:42:10 -0600

    Completed fizzbuzz challenge

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Almostenger.FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string output = "";
+
+                if (i % 3 == 0)
+                {
+                    output = "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";  // output = output + "Buzz";
+                }
+
+                if (output.Length == 0)
+                {
+                    output = i.ToString();
+                }
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
