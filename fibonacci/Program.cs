88a9c4d6a35cf7fa71f1b81d427730da7bcb967a
Kenny R <bitsecondal@gmail.com>
2021-06-04 11:42:01 -0500

    Closed #1 created Fibonacci program (#7)

---
+++ fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Almostengr.CodingChallenge.Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 twoNumbersAgo = 0;
+            Int64 oneNumberAgo = 1;
+            Int64 currentNumber = 0;
+
+            Console.WriteLine("How many Fibonacci sequence iterations would you like?");
+            int iterations = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("{0} + {1} = {2}", 0, 0, 0);
+
+            for (int i = 0; i < iterations-1; i++)
+            {
+                currentNumber = oneNumberAgo + twoNumbersAgo;
+
+                Console.WriteLine("{0} + {1} = {2}", twoNumbersAgo, oneNumberAgo, currentNumber);
+
+                twoNumbersAgo = oneNumberAgo;
+                oneNumberAgo = currentNumber;
+            }
+        }
+    }
+}
