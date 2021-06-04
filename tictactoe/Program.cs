8ba1b5c865c69f2bd67cc3bdbc17390dd0d0936e
Almost Engineer <bitsecondal@gmail.com>
2021-06-04 18:54:25 -0500

    #5 Created tic tac toe project

---
+++ tictactoe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Almostengr.CodingChallenge.TicTacToe
+{
+    class Program
+    {
+            static string[,] tttGrid = new string[3,3] {
+                { string.Empty, string.Empty, string.Empty },
+                { string.Empty, string.Empty, string.Empty },
+                { string.Empty, string.Empty, string.Empty }
+            };
+
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+        
+        private static void PrintGrid()
+        {
+            for(int i = 0; i < tttGrid.Length; i++)
+            {
+                for(int j = 0; j < tttGrid.)
+            }
+        }
+
+
+        private static bool CheckWinner()
+        {
+
+
+            return false;
+        }
+    }
+}
