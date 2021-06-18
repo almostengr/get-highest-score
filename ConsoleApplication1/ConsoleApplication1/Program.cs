661daf4d5ecd421f43e61ac1ec8b4e6dc91c64f0
almostengr <bitsecondal@gmail.com>
2021-06-18 15:33:15 -0500

    Added code from old project

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            for (i=1; i <= 20; i++)
+            {
+                // i = i + 1; // will increment numbers by two. 
+
+                Console.WriteLine(i);
+            }
+
+            // System("pause");
+            // Console.Readline();
+        }
+    }
+}
